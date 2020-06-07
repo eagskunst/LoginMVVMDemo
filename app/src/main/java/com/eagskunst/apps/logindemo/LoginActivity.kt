3fe0255fe1b59fc5d793a085ca6c1f511a49900b
Emmanuel Guerra <emmanuelnypd@gmail.com>
2020-06-07 11:11:59 -0400

    Initial commit

---
+++ app/src/main/java/com/eagskunst/apps/logindemo/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.eagskunst.apps.logindemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
