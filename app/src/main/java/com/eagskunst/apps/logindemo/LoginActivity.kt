006b886995aa3e3ee7d83a120ace371e3f2b5b78
Emmanuel Guerra <emmanuelnypd@gmail.com>
2020-06-07 11:50:20 -0400

    Styles for login activity

--- app/src/main/java/com/eagskunst/apps/logindemo/LoginActivity.kt
+++ app/src/main/java/com/eagskunst/apps/logindemo/LoginActivity.kt
@@ -2,13 +2,25 @@ package com.eagskunst.apps.logindemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import com.eagskunst.apps.logindemo.databinding.ActivityLoginBinding
+import com.eagskunst.apps.logindemo.utils.textString
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_login)
+        binding.usernameEt.doAfterTextChanged { changeButtonState() }
+        binding.passwordEt.doAfterTextChanged { changeButtonState() }
+    }
+
+    private fun changeButtonState() {
+        binding.loginBtn.isEnabled = binding.usernameEt.textString().isNotEmpty() &&
+                binding.passwordEt.textString().isNotEmpty()
     }
 }
---
+++ app/src/main/java/com/eagskunst/apps/logindemo/utils/Extensions.kt
@@ -0,0 +1,9 @@
+package com.eagskunst.apps.logindemo.utils
+
+import android.widget.EditText
+
+/**
+ * Created by eagskunst in 7/6/2020.
+ */
+
+fun EditText.textString() = this.text.toString()
